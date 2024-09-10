56a2e1e76b7f1e93dd79fb327af216f2297fecc8
Snehasish <snehasishmohanty9439@gmail.com>
2024-09-10 13:55:44 +0530

    Finished-frontend

--- vite-project/src/components/Main/Main.jsx
+++ vite-project/src/components/Main/Main.jsx
@@ -1,40 +1,56 @@
-import React from 'react'
-import './Main.css'
-import {assets} from '../../assets/assets'
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
 
 const Main = () => {
   return (
-    <div className='main'>
-        <div className="nav">
-          <p>Gemini</p>
-          <img src={assets.user_icon} alt="" />
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev</span>
+          </p>
+          <p>How can I help you today?</p>
         </div>
-        <div className="main-container">
-          <div className="greet">
-            <p><span>Hello, Dev</span></p>
-            <p>How can I help you today?</p>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <i className="fa-regular fa-compass"></i>
           </div>
-          <div className="cards">
-            <div className="card">
-              <p>Suggest beautiful places to see on an upcoming road trip</p>
-              <i class="fa-regular fa-compass"></i>
-            </div>
-            <div className="card">
-              <p>Suggest beautiful places to see on an upcoming road trip</p>
-              <i class="fa-regular fa-compass"></i>
-            </div>
-            <div className="card">
-              <p>Suggest beautiful places to see on an upcoming road trip</p>
-              <i class="fa-regular fa-compass"></i>
-            </div>
-            <div className="card">
-              <p>Suggest beautiful places to see on an upcoming road trip</p>
-              <i class="fa-regular fa-compass"></i>
-            </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <i className="fa-regular fa-lightbulb"></i>
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <i className="fa-regular fa-message"></i>
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <i className="fa-solid fa-code"></i>
           </div>
         </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter prompt here" />
+            <div>
+              <i class="fa-regular fa-images"></i>
+              <i class="fa-solid fa-microphone"></i>
+              <i class="fa-regular fa-paper-plane"></i>
+            </div>
+          </div>
+          <p className="bottom-info">
+            ©copyright - snehasishmohanty9439@gmail.com
+        </p>
+        </div>
+       
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
