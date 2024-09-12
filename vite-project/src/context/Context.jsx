71d9f7568baece9e23620efb50dfa01c30113311
Snehasish <snehasishmohanty9439@gmail.com>
2024-09-12 21:30:21 +0530

    Remove-double-star

--- vite-project/src/context/Context.jsx
+++ vite-project/src/context/Context.jsx
@@ -12,6 +12,10 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
+    const delayPara = (index, nextWord) => {
+
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
@@ -19,7 +23,17 @@ const ContextProvider = (props) => {
         setShowResult(true)
         setRecentPrompt(input)
         const response = await runChat(input)
-        setResultData(response)
+        let responseArray = response.split("**")
+        let newResponse ;
+        for(let i=0; i < responseArray.length; i++){
+            if(i===0 || i%2 !== 1){
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>" + responseArray[i]+ "</b>";
+            }
+        }
+        setResultData(newResponse)
         setLoading(false)
         setInput("")
     }
